688d2db4a3632f9413b7abff1a836d81d82082ab
Nvarma031 <nvarma031@gmail.com>
2022-10-06 06:32:42 +0000

    media queries

---
+++ index.js
@@ -0,0 +1,19 @@
+const element = (
+  <div className="bg-container">
+    <h1 className="heading">Super over league</h1>
+
+    <div className="image">
+      <img
+        class="image"
+        src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+      />
+
+      <img
+        class="image"
+        src="https://assets.ccbp.in/frontend/react-js/csk-img.png"
+      />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
